d97bc3f0e7e53d7c38d56c578c6bb2b86ac56369
thesamplepg <thesamplpg@gmail.com>
2019-06-04 01:28:14 +0600

    changed post /marks to get /marks

--- api/marks/index.js
+++ api/marks/index.js
@@ -1,7 +1,7 @@
 const router = require("express").Router();
 const controllers = require("../../controllers/marks");
 
-router.post("/", controllers.getAllMarks);
+router.get("/", controllers.getAllMarks);
 router.post("/create", controllers.createMark);
 
 module.exports = router;
