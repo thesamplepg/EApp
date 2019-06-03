92b2a575c39dfaf0f0e176d66d0f66f93b97233f
thesamplepg <thesamplpg@gmail.com>
2019-06-04 00:27:10 +0600

    fix bug with decodeJwt

--- utilits.js
+++ utilits.js
@@ -3,19 +3,15 @@ const jwt = require("jsonwebtoken");
 const authRequired = ["/api/marks/create"];
 
 exports.decodeJWT = (req, res, next) => {
-	jwt.verify(
-		req.cookies && req.cookies.token,
-		process.env.TOKEN_SECRET,
-		(err, data) => {
-			if (err && authRequired.indexOf(req.url) > -1) {
-				return res.status(401).json({ authorization: false });
-			}
-
-			console.log("works");
-			req.url = data;
-			next();
+	jwt.verify(req.cookies.token, process.env.TOKEN_SECRET, (err, data) => {
+		if (err && authRequired.indexOf(req.url) > -1) {
+			return res.status(401).json({ authorization: false });
 		}
-	);
+
+		req.user = data;
+	});
+
+	next();
 };
 
 exports.logger = (req, res, next) => {
