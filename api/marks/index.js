2e022a2cc68907d45c5c954a9bf1bae3814e47fe
thesamplepg <thesamplpg@gmail.com>
2019-06-04 01:07:05 +0600

    get all marks route

--- api/marks/index.js
+++ api/marks/index.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
 const controllers = require("../../controllers/marks");
 
+router.post("/", controllers.getAllMarks);
 router.post("/create", controllers.createMark);
 
 module.exports = router;
